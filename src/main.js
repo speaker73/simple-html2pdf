9a00b7f0480023428e75d18db0dc062cdc7ae8b1
speaker73 <alisa8523>
2019-05-22 16:29:58 +0300

    update jsPdf add .html() method

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const html2canvas = require('html2canvas');
 const jsPDF = require('jspdf');
-
+window.html2canvas = html2canvas;
 module.exports = function () {
   return function (element, options, callback) {
     const defaultOptions = {
@@ -69,46 +69,13 @@ module.exports = function () {
       }
     }
 
-    html2canvas(freeElement).then(function (canvas) {
-      const pdf = new jsPDF('p', 'px', 'a4');
-      const canvasWidth = canvas.width;
-      const canvasHeight = canvas.height;
-      const pageWidth = pdf.internal.pageSize.getWidth();
-      const pageHeight = pdf.internal.pageSize.getHeight();
-
-      const pageMargin = Number.parseInt(options.margin * pageWidth / BEST_WIDTH);
-      const imgWidth = pageWidth - 2 * pageMargin;
-      const imgHeight = pageHeight - 2 * pageMargin;
-
-      let addImageWH = false;
-      if (canvasWidth <= BEST_ELEMENT_WIDTH) addImageWH = true;
-
-      let position = 0;
-      while (position <= canvasHeight) {
-        const tempCanvas = document.createElement('canvas');
-        const tempHeight = Math.max(BEST_ELEMENT_WIDTH, canvasWidth) * imgHeight / imgWidth;
-        tempCanvas.width = canvasWidth;
-        tempCanvas.height = tempHeight;
-        const tempCtx = tempCanvas.getContext('2d');
-        const bodyCtx = canvas.getContext('2d');
-        const imgData = bodyCtx.getImageData(0, position, canvasWidth, tempHeight);
-        tempCtx.putImageData(imgData, 0, 0, 0, 0, canvasWidth, tempHeight);
-
-        const tempCanvasData = tempCanvas.toDataURL("image/png");
-        if (addImageWH) {
-          pdf.addImage(tempCanvasData, 'PNG', pageMargin, pageMargin);
-        } else {
-          pdf.addImage(tempCanvasData, 'PNG', pageMargin, pageMargin, imgWidth, imgHeight);
-        }
-
-        position += tempHeight;
-        if (position <= canvasHeight) {
-          pdf.addPage();
-        }
+    pdf.html(freeElement, {
+      callback:function(){
+        pdf.save(options.filename);
+        if (printFF) document.body.removeChild(printFF);
+        if (typeof callback === 'function') callback();
       }
-      pdf.save(options.filename);
-      if (printFF) document.body.removeChild(printFF);
-      if (typeof callback === 'function') callback();
     });
+
   }
 };
